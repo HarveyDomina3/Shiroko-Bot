0df2b46b1ea0bfbbf3c52a7c54fc668d70ca0bcf
HarveyDomina3 <157261006+HarveyDomina3@users.noreply.github.com>
2025-06-24 15:07:41 -0400

    Update fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -1,14 +1,15 @@
 import hispamemes from 'hispamemes'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', fkontak)
-m.react(emoji2)
+
+let handler = async (m, { conn }) => {
+    const meme = await hispamemes.meme()
+    conn.sendFile(m.chat, meme, '', '', m)
+    m.react('🤣')
 }
+
 handler.help = ['meme']
 handler.tags = ['fun']
 handler.command = ['meme', 'memes']
-handler.coin = 1
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
